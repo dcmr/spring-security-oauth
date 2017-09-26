83d74cb97786cdb8b80eb5a099d71fa26dc684c6
keets2012 <928420597@qq.com>
2017-09-26 15:08:32 +0800

    add interface

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerEndpointsConfiguration.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerEndpointsConfiguration.java
@@ -118,6 +118,7 @@ public class AuthorizationServerEndpointsConfiguration {
 	@Bean
 	public CheckTokenEndpoint checkTokenEndpoint() {
 		CheckTokenEndpoint endpoint = new CheckTokenEndpoint(getEndpointsConfigurer().getResourceServerTokenServices());
+		endpoint.setCheckPermissions(checkPermissions);
 		endpoint.setAccessTokenConverter(getEndpointsConfigurer().getAccessTokenConverter());
 		endpoint.setExceptionTranslator(exceptionTranslator());
 		return endpoint;
